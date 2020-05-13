d746486a2b30efd07d057687a638d8483503881c
Alex <alex.naga@postnord.com>
2020-05-13 13:31:36 +0200

    Add ESLint

--- src/components/tiqqe-button/tiqqe-button.tsx
+++ src/components/tiqqe-button/tiqqe-button.tsx
@@ -1,19 +1,19 @@
-import { Component, Prop, h } from '@stencil/core'
+import { Component, Prop, h } from '@stencil/core';
 
 @Component({
-  tag: 'tiqqe-button',
-  styleUrl: 'tiqqe-button.scss',
-  shadow: true
+    tag: 'tiqqe-button',
+    styleUrl: 'tiqqe-button.scss',
+    shadow: true
 })
 
 export class ButtonComponent {
   @Prop() color: 'plain' | 'primary' | 'secondary' | 'danger' = 'plain'
 
   render() {
-    return (
-      <button class={`${this.color}`}>
-        <slot />
-      </button>
-    )
+      return (
+          <button class={`${this.color}`}>
+              <slot />
+          </button>
+      );
   }
 }
