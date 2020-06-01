4f2d16cf8861aad93d507befb93a48a132fd4d1a
AlexNaga <alex.naga@hotmail.com>
2020-06-01 14:06:04 +0000

    Deploying to gh-pages from  @ 465c56bfa6276f842aee1c50ccc1cd281611fd99 🚀

--- build/p-8d1a538a.system.js
+++ build/p-8d1a538a.system.js
@@ -1,1 +1,1 @@
-System.register(["./p-e3ed9582.system.js"],(function(){"use strict";var e,t;return{setters:[function(n){e=n.p;t=n.b}],execute:function(){e().then((function(e){return t([["p-06a9ed5e.system",[[1,"tiqqe-button",{label:[1],color:[1]}]]]],e)}))}}}));
+System.register(["./p-e3ed9582.system.js"],(function(){"use strict";var e,t;return{setters:[function(n){e=n.p;t=n.b}],execute:function(){e().then((function(e){return t([["p-06a9ed5e.system",[[1,"tiqqe-button",{color:[1],label:[1]}]]]],e)}))}}}));
