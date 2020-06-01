610e76125360743e59ca7faf1af4be522688646c
AlexNaga <alex.naga@hotmail.com>
2020-06-01 11:38:30 +0000

    Deploying to gh-pages from  @ 34be738daa05f285e39198420f86f4146ca48739 🚀

--- esm/storybook-stencil.mjs
+++
@@ -1,5 +0,0 @@
-import { p as patchBrowser, b as bootstrapLazy } from './index-be579b61.js';
-
-patchBrowser().then(options => {
-  return bootstrapLazy([["tiqqe-button",[[1,"tiqqe-button",{"label":[1],"color":[1]}]]]], options);
-});
--- types/components/buttons/tiqqe-button/tiqqe-button.d.ts
+++
@@ -1,5 +0,0 @@
-export declare class ButtonComponent {
-    label: string;
-    color: 'primary' | 'secondary' | 'danger' | 'success';
-    render(): any;
-}
